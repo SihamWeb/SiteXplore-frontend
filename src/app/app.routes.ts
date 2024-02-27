373276ea9e2b702e63250692b7bdc4c1d7bf810a
SihamWeb <siham.charef@etu.unilim.fr>
2024-02-27 11:43:52 +0100

    creation-pages: Add register page && service files

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,14 @@
-import { Routes } from '@angular/router';
-import{ HomeComponent } from "./home/home.component";
+import {Routes} from '@angular/router';
+import {HomeComponent} from "./home/home.component";
 import {ContactComponent} from "./contact/contact.component";
 import {MemberSpaceComponent} from "./member-space/member-space.component";
 import {LogInComponent} from "./log-in/log-in.component";
+import {RegisterComponent} from "./register/register.component";
 
 export const routes: Routes = [
   { path : '', component : HomeComponent },
   { path : 'contact', component : ContactComponent },
   { path : 'mon-compte', component : MemberSpaceComponent },
-  { path : 'connexion', component : LogInComponent }
+  { path : 'connexion', component : LogInComponent },
+  { path : 'inscription', component : RegisterComponent }
 ];
