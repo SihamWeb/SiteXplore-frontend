bf1c4030157f338926cbf5a316f14aeab618c1b0
SihamWeb <siham.charef@etu.unilim.fr>
2024-02-27 11:51:51 +0100

    creation-pages: Add quiz page && service files

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ import {MemberSpaceComponent} from "./member-space/member-space.component";
 import {LogInComponent} from "./log-in/log-in.component";
 import {RegisterComponent} from "./register/register.component";
 import {ForgottenPasswordComponent} from "./forgotten-password/forgotten-password.component";
+import {QuizComponent} from "./quiz/quiz.component";
 
 export const routes: Routes = [
   { path : '', component : HomeComponent },
@@ -12,5 +13,6 @@ export const routes: Routes = [
   { path : 'mon-compte', component : MemberSpaceComponent },
   { path : 'connexion', component : LogInComponent },
   { path : 'inscription', component : RegisterComponent },
-  { path : 'mot-de-passe-oublie', component : ForgottenPasswordComponent }
+  { path : 'mot-de-passe-oublie', component : ForgottenPasswordComponent },
+  { path : 'quiz', component : QuizComponent }
 ];
