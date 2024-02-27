fd46c09e65d9908c6587567d9ff5c4465f8fe6ce
SihamWeb <siham.charef@etu.unilim.fr>
2024-02-27 11:28:22 +0100

    creation-pages: Add member space page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import{ HomeComponent } from "./home/home.component";
 import {ContactComponent} from "./contact/contact.component";
+import {MemberSpaceComponent} from "./member-space/member-space.component";
 
 export const routes: Routes = [
   { path : '', component : HomeComponent },
-  { path : 'contact', component : ContactComponent }
+  { path : 'contact', component : ContactComponent },
+  { path : 'mon-compte', component : MemberSpaceComponent }
 ];
