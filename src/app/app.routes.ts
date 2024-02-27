a77608f0f6047796104df08751e78b33ec0647c7
SihamWeb <siham.charef@etu.unilim.fr>
2024-02-27 11:38:45 +0100

    creation-pages: Add log-in page && service files

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import{ HomeComponent } from "./home/home.component";
 import {ContactComponent} from "./contact/contact.component";
 import {MemberSpaceComponent} from "./member-space/member-space.component";
+import {LogInComponent} from "./log-in/log-in.component";
 
 export const routes: Routes = [
   { path : '', component : HomeComponent },
   { path : 'contact', component : ContactComponent },
-  { path : 'mon-compte', component : MemberSpaceComponent }
+  { path : 'mon-compte', component : MemberSpaceComponent },
+  { path : 'connexion', component : LogInComponent }
 ];
