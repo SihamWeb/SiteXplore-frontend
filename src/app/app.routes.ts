cfa8405e9bb3b8378be411622ac80b254a501519
SihamWeb <siham.charef@etu.unilim.fr>
2024-02-27 11:46:45 +0100

    creation-pages: Add forgotten-password page && service files

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,13 @@ import {ContactComponent} from "./contact/contact.component";
 import {MemberSpaceComponent} from "./member-space/member-space.component";
 import {LogInComponent} from "./log-in/log-in.component";
 import {RegisterComponent} from "./register/register.component";
+import {ForgottenPasswordComponent} from "./forgotten-password/forgotten-password.component";
 
 export const routes: Routes = [
   { path : '', component : HomeComponent },
   { path : 'contact', component : ContactComponent },
   { path : 'mon-compte', component : MemberSpaceComponent },
   { path : 'connexion', component : LogInComponent },
-  { path : 'inscription', component : RegisterComponent }
+  { path : 'inscription', component : RegisterComponent },
+  { path : 'mot-de-passe-oublie', component : ForgottenPasswordComponent }
 ];
