38e45e8e8669d945cd05afacabab456dd9c4910c
mxrlxw <121444190+nsa-dev@users.noreply.github.com>
2024-03-04 17:26:17 +0100

    Layout: Header + Menu (sample header image)

--- src/app/sections/menu/menu.component.ts
+++ src/app/sections/menu/menu.component.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive, RouterOutlet, RouterModule } from '@angular/router';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { faBars, faUser, faSearch } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [FontAwesomeModule],
+  imports: [FontAwesomeModule, RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.scss'
 })
 export class MenuComponent {
   faBars = faBars;
+  faUser = faUser;
+  faSearch = faSearch;
 }
