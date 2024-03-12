94b481fcea020840708e64745c4e43417d5c6888
mxrlxw <121444190+nsa-dev@users.noreply.github.com>
2024-03-12 20:33:55 +0100

    Layout: fixes + login/register WIP

--- src/app/log-in/log-in.component.ts
+++ src/app/log-in/log-in.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from '../sections/footer/footer.component';
+import { HeaderComponent } from '../sections/header/header.component';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-log-in',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent, FooterComponent, RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './log-in.component.html',
   styleUrl: './log-in.component.scss'
 })
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../sections/header/header.component';
+import { FooterComponent } from '../sections/footer/footer.component';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent, FooterComponent, RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
