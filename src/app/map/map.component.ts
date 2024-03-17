8af4ac0d139d0978524f47c5c47a188662b805d8
mxrlxw <121444190+nsa-dev@users.noreply.github.com>
2024-03-17 17:34:53 +0100

    Layout: map + article-feed

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,6 +1,9 @@
-import { Component } from '@angular/core';
 import { HeaderComponent } from '../sections/header/header.component';
 import { FooterComponent } from '../sections/footer/footer.component';
+import * as L from 'leaflet';
+import { Component, OnInit, Inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+
 @Component({
   selector: 'app-map',
   standalone: true,
@@ -8,6 +11,15 @@ import { FooterComponent } from '../sections/footer/footer.component';
   templateUrl: './map.component.html',
   styleUrl: './map.component.scss'
 })
-export class MapComponent {
+
+
+export class MapComponent implements OnInit {
   title = 'Carte intéractive';
+
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) { }
+
+  ngOnInit(): void {
+    if (isPlatformBrowser(this.platformId)) {
+    }
+  }
 }
