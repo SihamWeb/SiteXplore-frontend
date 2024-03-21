db0f0ced704442eadc0efccd96e1478b725c035e
mxrlxw <121444190+nsa-dev@users.noreply.github.com>
2024-03-21 18:48:18 +0100

    Layout: WIP

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../sections/header/header.component';
+import { FooterComponent } from '../sections/footer/footer.component';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent, FooterComponent],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
